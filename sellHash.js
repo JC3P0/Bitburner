095a1ee9f7d7fc8c8d0bf75348c79fec5e91623e
Josh Clemens <josh.clemens@outlook.com>
2024-08-05 10:04:06 -0700

    update sellHash.js

--- sellHash.js
+++ sellHash.js
@@ -1,15 +1,13 @@
 /** @param {NS} ns */
 export async function main(ns) {
-    while (true) {
-      var numOfHashes = await ns.hacknet.numHashes();
-      var maxHashes = await ns.hacknet.hashCapacity();
-      //(0.9 * maxHashes) or 4 (max or min)
+  while (true) {
+      const numOfHashes = ns.hacknet.numHashes();
+      const maxHashes = ns.hacknet.hashCapacity();
+      // (0.9 * maxHashes) or 4 (max or min)
       if (numOfHashes >= (0.9 * maxHashes)) {
-        await ns.hacknet.spendHashes("Sell for Money", "")
+          await ns.hacknet.spendHashes("Sell for Money", "");
+      } else {
+          await ns.sleep(2000);
       }
-      else {
-        await ns.sleep(2000);
-      }
-    }
   }
-  
+}
