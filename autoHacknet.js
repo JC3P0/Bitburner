2c8a4aa222f248fdd09391eb02e995e1fc267209
Josh Clemens <josh.clemens@outlook.com>
2024-08-05 10:00:18 -0700

    update autoHacknet.js

--- autoHacknet.js
+++ autoHacknet.js
@@ -1,50 +1,47 @@
 /** @param {NS} ns */
 export async function main(ns) {
-    const maxNumOfNodes = ns.hacknet.maxNumNodes();
-    while(true){
-      var numOfNodes = await ns.hacknet.numNodes();
-      var moneyAvailable = await ns.getServerMoneyAvailable("home");
-      var nodeCost = await ns.hacknet.getPurchaseNodeCost();
-  
-      while(numOfNodes == 0) {
-        if(moneyAvailable >= nodeCost) {
-          await ns.hacknet.purchaseNode();
-          numOfNodes += 1;
-          moneyAvailable -= nodeCost;
-        }
-        else {
-          ns.sleep(2000);
-        }
+  const maxNumOfNodes = ns.hacknet.maxNumNodes();
+
+  while (true) {
+      let numOfNodes = ns.hacknet.numNodes();
+      let moneyAvailable = ns.getServerMoneyAvailable("home");
+      let nodeCost = ns.hacknet.getPurchaseNodeCost();
+
+      while (numOfNodes === 0) {
+          if (moneyAvailable >= nodeCost) {
+              ns.hacknet.purchaseNode();
+              numOfNodes += 1;
+              moneyAvailable -= nodeCost;
+          } else {
+              await ns.sleep(2000);
+          }
+          nodeCost = ns.hacknet.getPurchaseNodeCost(); // Update nodeCost in case it changes
       }
-  
-      for(let i=0; i<numOfNodes; i++) {
-        var nodeStats = await ns.hacknet.getNodeStats(i);
-  
-        var coreCost = await ns.hacknet.getCoreUpgradeCost(i);
-        var ramCost = await ns.hacknet.getRamUpgradeCost(i);
-        var levelCost = await ns.hacknet.getLevelUpgradeCost(i);
-  
-        nodeCost = await ns.hacknet.getPurchaseNodeCost();
-  
-        if(nodeCost <= moneyAvailable && numOfNodes < maxNumOfNodes) {
-          await ns.hacknet.purchaseNode();
-          moneyAvailable -= nodeCost;
-          numOfNodes += 1;
-        }
-        else if(coreCost <= moneyAvailable && nodeStats.cores < 16) {
-          await ns.hacknet.upgradeCore(i);
-          moneyAvailable -= coreCost;
-        }
-        else if(ramCost <= moneyAvailable) {
-          await ns.hacknet.upgradeRam(i);
-          moneyAvailable -= ramCost;
-        }
-        else if(levelCost <= moneyAvailable && nodeStats.level < 200) {
-          await ns.hacknet.upgradeLevel(i);
-          moneyAvailable -= levelCost;
-        }
+
+      for (let i = 0; i < numOfNodes; i++) {
+          const nodeStats = ns.hacknet.getNodeStats(i);
+
+          const coreCost = ns.hacknet.getCoreUpgradeCost(i);
+          const ramCost = ns.hacknet.getRamUpgradeCost(i);
+          const levelCost = ns.hacknet.getLevelUpgradeCost(i);
+
+          nodeCost = ns.hacknet.getPurchaseNodeCost(); // Update nodeCost in case it changes
+
+          if (nodeCost <= moneyAvailable && numOfNodes < maxNumOfNodes) {
+              ns.hacknet.purchaseNode();
+              moneyAvailable -= nodeCost;
+              numOfNodes += 1;
+          } else if (coreCost <= moneyAvailable && nodeStats.cores < 16) {
+              ns.hacknet.upgradeCore(i);
+              moneyAvailable -= coreCost;
+          } else if (ramCost <= moneyAvailable) {
+              ns.hacknet.upgradeRam(i);
+              moneyAvailable -= ramCost;
+          } else if (levelCost <= moneyAvailable && nodeStats.level < 200) {
+              ns.hacknet.upgradeLevel(i);
+              moneyAvailable -= levelCost;
+          }
       }
       await ns.sleep(2000);
-    }
   }
-  
+}
