c0235e9fd90c1fa05552f135ddff7c841136ec17
Josh Clemens <josh.clemens@outlook.com>
2024-08-05 10:02:32 -0700

    update purchaseServer.js

--- purchaseServer.js
+++ purchaseServer.js
@@ -1,81 +1,75 @@
-/** @param {NS} ns */
+/** @param {NS} ns **/
 export async function main(ns) {
-    var target = ns.args[0];
-    var home = "home";
-    var pRam = 8;
-    // var servPrefix = "PurchasedServer(";
-    var servPrefix = "(J)$$$(";
-  
-    var maxRam = ns.getPurchasedServerMaxRam();
-    var maxServers = ns.getPurchasedServerLimit();
-  
-    var virus = "getMoney.js";
-    var virusRam = ns.getScriptRam(virus);
-  
-    function canPurchaseServer(){
+  const target = ns.args[0] || "foodnstuff"; // Default target to 'foodnstuff' if not provided
+  const home = "home";
+  let pRam = 8;
+  const servPrefix = "(J)$$$(";
+
+  const maxRam = ns.getPurchasedServerMaxRam();
+  const maxServers = ns.getPurchasedServerLimit();
+
+  const virus = "getMoney.js";
+  const virusRam = ns.getScriptRam(virus);
+
+  function canPurchaseServer() {
       return ns.getServerMoneyAvailable(home) > ns.getPurchasedServerCost(pRam);
-    }
-  
-    function killVirus(server){
-      if(ns.scriptRunning(virus, server)){
-        ns.scriptKill(virus, server);
+  }
+
+  function killVirus(server) {
+      if (ns.scriptRunning(virus, server)) {
+          ns.scriptKill(virus, server);
       }
-    }
-  
-    async function copyVirus(server){
+  }
+
+  async function copyVirus(server) {
       ns.print("virus-> " + server);
       await ns.scp(virus, server);
       killVirus(server);
-      var maxThreads = Math.floor(pRam / virusRam);
+      const maxThreads = Math.floor(pRam / virusRam);
       ns.exec(virus, server, maxThreads, target);
-    }
-  
-    function shutdownServer(server){
+  }
+
+  function shutdownServer(server) {
       killVirus(server);
       ns.deleteServer(server);
-    }
-  
-    async function upgradeServer(server){
-      var sRam = ns.getServerMaxRam(server);
-      if(sRam < pRam){
-        while(!canPurchaseServer()){
-          await ns.sleep(10000); //wait 10 sec, until you have enough money to buy server
-        }
-        shutdownServer(server);
-        ns.purchaseServer(server, pRam);
+  }
+
+  async function upgradeServer(server) {
+      const sRam = ns.getServerMaxRam(server);
+      if (sRam < pRam) {
+          while (!canPurchaseServer()) {
+              await ns.sleep(10000); // wait 10 sec until you have enough money to buy server
+          }
+          shutdownServer(server);
+          ns.purchaseServer(server, pRam);
       }
       await copyVirus(server);
-    }
-  
-    async function autoUpgradeServers(){
-      var i = 1;
-      while(i < (maxServers + 1)){
-        var server = servPrefix + i.toString().padStart(2, '0') + ")";
-        if(ns.serverExists(server)){
-          ns.print("Upgrading: " + server + " (" + pRam + "GB)");
-          await upgradeServer(server);
-          ++i;
-        } else if (canPurchaseServer()) {
-          ns.print("Buying: " + server + " (" + pRam + "GB)");
-          ns.purchaseServer(server, pRam);
-          await copyVirus(server);
-          ++i;
-        }
-      }
-    }
-  
-    while(true){
-      await autoUpgradeServers();
-      if(pRam === maxRam){
-        break;
-      }
-      //move up a tier
-      var newRam = pRam * 2;
-      if (newRam > maxRam){
-        pRam = maxRam;
-      } else {
-        pRam = newRam;
-      }
-    }
   }
-  
+
+  async function autoUpgradeServers() {
+      let i = 1;
+      while (i <= maxServers) {
+          const server = `${servPrefix}${i.toString().padStart(2, '0')})`;
+          if (ns.serverExists(server)) {
+              ns.print("Upgrading: " + server + " (" + pRam + "GB)");
+              await upgradeServer(server);
+              i++;
+          } else if (canPurchaseServer()) {
+              ns.print("Buying: " + server + " (" + pRam + "GB)");
+              ns.purchaseServer(server, pRam);
+              await copyVirus(server);
+              i++;
+          }
+      }
+  }
+
+  while (true) {
+      await autoUpgradeServers();
+      if (pRam === maxRam) {
+          break;
+      }
+      // move up a tier
+      const newRam = pRam * 2;
+      pRam = newRam > maxRam ? maxRam : newRam;
+  }
+}
